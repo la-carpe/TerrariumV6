ce510b2562f9e1e8b5ee419ae8b5b2687ca49122
la-carpe <64590614+la-carpe@users.noreply.github.com>
2021-05-15 00:08:19 +0200

    pleins de chose

--- Terrarium_App/urls.py
+++ Terrarium_App/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('home.html', views.home),
     path('', views.home),
+    path('graphiques.html', views.graphiques)
+
+
 ]
--- Terrarium_App/views.py
+++ Terrarium_App/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home (request):
     return render(request, 'home.html', {})
+
+def graphiques (request):
+    return render(request, 'graphiques.html', {})
+
